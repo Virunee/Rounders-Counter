fc6315abf52d07ecd51dcfec5c37025bf4c6b072
Virunee <viravongv@gmail.com>
2017-03-02 16:50:22 +0000

    Team A functionality
    Added all required buttons for functionality to keep track of score for
    Team A

--- app/src/main/java/uk/co/virunee/rounderscounter/MainActivity.java
+++ app/src/main/java/uk/co/virunee/rounderscounter/MainActivity.java
@@ -2,6 +2,10 @@ package uk.co.virunee.rounderscounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +13,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(0.0);
+        displayForTeamAOut(0);
     }
 
-    
+    double team_a_score = 0;
+    int team_a_outs = 0;
+
+    public void addOneRounderA(View view) {
+        team_a_score = team_a_score + 1;
+        displayForTeamA(team_a_score);
+    }
+
+    public void addHalfRounderA(View view) {
+        team_a_score = team_a_score + 0.5;
+        displayForTeamA(team_a_score);
+    }
+
+    public void addOneOutA(View view) {
+        team_a_outs = team_a_outs + 1;
+        displayForTeamAOut(team_a_outs);
+    }
+
+
+    //Displays the given score for Team A.
+
+    public void displayForTeamA(double team_a_score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(team_a_score));
+    }
+
+    public void displayForTeamAOut(int team_a_outs) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_outs);
+        scoreView.setText(String.valueOf(team_a_outs));
+    }
+
+    // OVERFLOW MENU STUFF //
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
 }
